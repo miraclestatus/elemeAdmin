5fafd739087d1b010ac8cab43f7b1d5ad5439c85
miraclestatus <31105601+miraclestatus@users.noreply.github.com>
2020-08-07 14:30:38 +0800

    1111

--- src/com/neusoft/ElmAdmin.java
+++ src/com/neusoft/ElmAdmin.java
@@ -26,8 +26,43 @@ public class ElmAdmin {
         Admin admin = adminView.login();
 
         if (admin!=null){
+            int menu = 0;
             System.out.println("~欢迎来到饿了么商家管理系统~");
-            //
+            while (menu!= 5){
+
+                // 创建一个菜单
+                System.out.println("========= 1.所有商家列表=2.搜索商家=3.新建商家=4.删除商家=5.退出系统 =========");
+                System.out.println("请选择相应的菜单编号");
+                menu = input.nextInt();
+
+                switch (menu){
+                    case 1:
+                        System.out.println("1.所有商家列表");
+                        break;
+                    case 2:
+                        System.out.println("1.搜索商家");
+                        break;
+                    case 3:
+                        System.out.println("1.新建商家");
+                        break;
+                    case 4:
+                        System.out.println("1.删除商家");
+                        break;
+                    case 5:
+                        System.out.println("========= 欢迎下次光临饿了么系统 =========");
+                        break;
+                    default:
+                        System.out.println("没有这个菜单项");
+                        break;
+                }
+
+            }
+
+
+
+
+        }else {
+            System.out.println("账号或密码有误请重新输入");
         }
 
     }
